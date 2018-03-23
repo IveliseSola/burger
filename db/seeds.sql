9e7acdb93d51e5ac6fe44930957407b8685e726d
Ivelise Sola <solaivelise@yahoo.com>
2018-03-23 15:02:33 -0400

    seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name) VALUES ('Vegetarian Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burgers_name) VALUES ('Lamb Burger');
