c975a79786b247ed85cc0ae4622959369017d74b
Ivelise Sola <solaivelise@yahoo.com>
2018-03-27 02:53:52 -0400

    changes to main.handlebars

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -12,5 +12,26 @@ $(function() {
           console.log("created new burger");
           location.reload();
         })
-    })
+    });
+
+    $(".devour-btn").on("click", function(event) {
+        var id = $(this).data("id");
+        var newSleep = $(this).data("newsleep");
+    
+        var newSleepState = {
+          sleepy: newSleep
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/cats/" + id, {
+          type: "PUT",
+          data: newSleepState
+        }).then(
+          function() {
+            console.log("changed sleep to", newSleep);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
 });
