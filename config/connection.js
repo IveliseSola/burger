423c5aa151e437511fe6cb1072482b05dc885183
Ivelise Sola <solaivelise@yahoo.com>
2018-04-10 14:26:14 -0400

    JawsDb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "127.0.0.1",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
 
 module.exports = connection;
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
